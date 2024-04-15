6bf9efde29f5dbb29d586713508e7b28382a6b75
Lekuru <contact@lekuru.xyz>
2024-04-16 00:06:43 +0200

    Add postgres config

--- config.py
+++ config.py
@@ -4,6 +4,12 @@ import os
 
 dotenv.load_dotenv(override=True)
 
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST', 'localhost')
+POSTGRES_PORT = int(os.environ.get('POSTGRES_PORT', '5432'))
+POSTGRES_DBNAME = os.environ.get('POSTGRES_DBNAME', 'postgres')
+POSTGRES_USER = os.environ.get('POSTGRES_USER', 'postgres')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+
 WEB_HOST = os.environ.get('WEB_HOST')
 WEB_PORT = int(os.environ.get('WEB_PORT', 80))
 
