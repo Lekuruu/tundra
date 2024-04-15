5241ef6a889a793a113fce444a1db01a5d14519d
Lekuru <contact@lekuru.xyz>
2024-04-16 00:25:35 +0200

    Add postcard repository

---
+++ app/data/repositories/postcard.py
@@ -0,0 +1,20 @@
+
+from __future__ import annotations
+from typing import List
+
+from sqlalchemy.orm import Session
+from sqlalchemy import func
+
+from .wrapper import session_wrapper
+from ..objects import Postcard
+
+@session_wrapper
+def fetch_by_id(postcard_id: int, session: Session = ...) -> Postcard | None:
+    return session.query(Postcard) \
+        .filter(Postcard.id == postcard_id) \
+        .first()
+
+@session_wrapper
+def fetch_all(penguin_id: int, session: Session = ...) -> List[Postcard]:
+    return session.query(Postcard) \
+        .all()
