364d3fb41b1697571af99c066c9faed271cef487
Lekuru <contact@lekuru.xyz>
2024-04-16 17:52:37 +0200

    Add sendgrid configuration

--- config.py
+++ config.py
@@ -10,8 +10,16 @@ POSTGRES_DBNAME = os.environ.get('POSTGRES_DBNAME', 'postgres')
 POSTGRES_USER = os.environ.get('POSTGRES_USER', 'postgres')
 POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
 
+SITE_NAME = os.environ.get('SITE_NAME', 'localhost')
+FROM_EMAIL = os.environ.get('FROM_EMAIL', 'noreply@localhost')
+SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY', '')
+MAX_ACCOUNT_EMAIL = int(os.environ.get('MAX_ACCOUNT_EMAIL', '5'))
+
 WEB_HOST = os.environ.get('WEB_HOST')
 WEB_PORT = int(os.environ.get('WEB_PORT', 80))
-
 STATIC_KEY = os.environ.get('STATIC_KEY', 'houdini')
+
+USERNAME_FORCE_CASE = os.environ.get('USERNAME_FORCE_CASE', 'false').lower() == 'true'
+APPROVE_USERNAME = os.environ.get('APPROVE_USERNAME', 'false').lower() == 'true'
+ACTIVATE_PLAYER = os.environ.get('ACTIVATE_PLAYER', 'false').lower() == 'true'
 DEBUG = os.environ.get('DEBUG', 'false').lower() == 'true'
