d0efc14fcbd7f3c661c804a11e16c91d51938633
Lekuru <contact@lekuru.xyz>
2024-04-16 17:16:25 +0200

    Add system language enum

--- app/data/constants.py
+++ app/data/constants.py
@@ -1,4 +1,49 @@
 
+from enum import IntEnum
+
+class SystemLanguage(IntEnum):
+    Afrikaans = 0
+    Arabic = 1
+    Basque = 2
+    Belarusian = 3
+    Bulgarian = 4
+    Catalan = 5
+    Chinese = 6
+    Czech = 7
+    Danish = 8
+    Dutch = 9
+    English = 10
+    Estonian = 11
+    Faroese = 12
+    Finnish = 13
+    French = 14
+    German = 15
+    Greek = 16
+    Hebrew = 17
+    Hugarian = 18
+    Icelandic = 19
+    Indonesian = 20
+    Italian = 21
+    Japanese = 22
+    Korean = 23
+    Latvian = 24
+    Lithuanian = 25
+    Norwegian = 26
+    Polish = 27
+    Portuguese = 28
+    Romanian = 29
+    Russian = 30
+    SerboCroatian = 31
+    Slovak = 32
+    Slovenian = 33
+    Spanish = 34
+    Swedish = 35
+    Thai = 36
+    Turkish = 37
+    Ukrainian = 38
+    Vietnamese = 39
+    Unknown = 40
+
 CELLOPHANE_TOKENS = {
     'Production': '9B407F96-418B-4E0B-93DB-3AD33CC7D72E:205EF7823B24EE5277E318E061E5557F4648F1BF4CCFB457',
     'Development': '0869F69C-56CC-4EEB-9D5E-D3887F971033:AF5DC9E77FB4FE15F8C051ABC544435A410B46B725CE94B5'
