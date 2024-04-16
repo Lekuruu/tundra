fafb858be4b0f9bbf7d5ea28b574bd33b7207a12
Lekuru <contact@lekuru.xyz>
2024-04-16 13:35:53 +0200

    Implement login time

--- app/routes/clubpenguin/mobile/v2/authentication.py
+++ app/routes/clubpenguin/mobile/v2/authentication.py
@@ -3,8 +3,8 @@ from starlette.authentication import requires
 from fastapi import APIRouter, Request
 from datetime import datetime
 
+from app.data import Penguin, logins
 from app.models import AuthData
-from app.data import Penguin
 from app import crypto
 
 router = APIRouter()
@@ -14,17 +14,24 @@ router = APIRouter()
 def authtoken_request(request: Request):
     user: Penguin = request.user
 
+    # Default login time
+    recent_login = datetime.now()
+
+    # Fetch recent login time
+    if (login := logins.fetch_recent(user.id)):
+        recent_login = login.date
+
     return AuthData(
         PlayerId=user.id,
         PlayerSwid=user.nickname,
         Username=user.username,
         DisplayName=user.display_name,
         AuthToken=crypto.generate_token(user),
-        LastLogin='lastlogin', # TODO
-        Member=True, # TODO
-        PendingActivation=(not user.active),
+        LastLogin=recent_login.strftime('%Y-%m-%d %H:%M:%S'),
         SaveMode=user.safe_chat,
-        AccountType='accounttype', # TODO
+        Member=True, # TODO
+        AccountType='normal', # TODO
+        PendingActivation=(not user.active),
         DaysLeft=(
             max(7 - (datetime.now() - user.registration_date).days, 0)
             if not user.active else None
