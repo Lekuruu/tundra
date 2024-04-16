f9b37528bf9a4b180ab2e867bfaf8f142ff88487
Lekuru <contact@lekuru.xyz>
2024-04-16 17:16:52 +0200

    Add `/account` endpoint

--- app/routes/clubpenguin/mobile/v2/__init__.py
+++ app/routes/clubpenguin/mobile/v2/__init__.py
@@ -2,6 +2,8 @@
 from fastapi import APIRouter
 
 from . import authentication
+from . import account
 
 router = APIRouter()
 router.include_router(authentication.router)
+router.include_router(account.router)
---
+++ app/routes/clubpenguin/mobile/v2/account.py
@@ -0,0 +1,36 @@
+
+from starlette.authentication import requires
+from fastapi import APIRouter, Request
+from datetime import datetime
+
+from app.data import Penguin, SystemLanguage
+from app.models import Account
+
+router = APIRouter()
+
+@router.get('/account')
+@requires('authenticated')
+def account(request: Request):
+    user: Penguin = request.user
+
+    return Account(
+        PlayerId=user.id,
+        PlayerSwid=user.nickname,
+        Username=user.username,
+        DisplayName=user.display_name,
+        Email=user.email,
+        PenguinAge=(datetime.now() - user.registration_date).days,
+        Colour=user.color,
+        Language=SystemLanguage.Unknown, # TODO
+        AccountType='normal', # TODO
+        Member=True, # TODO
+        LapsedMember=False, # TODO
+        PendingActivation=(not user.active),
+        SafeMode=user.safe_chat,
+        Recurring=False, # TODO
+        DaysAsMember=30, # TODO
+        DaysLeft=(
+            max(7 - (datetime.now() - user.registration_date).days, 0)
+            if not user.active else None
+        )
+    )
