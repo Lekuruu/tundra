9e0a597f96cbe37902dbf1ad41b03c4349a62cd2
Lekuru <contact@lekuru.xyz>
2025-01-09 22:31:19 +0100

    Add directory routing

--- app/routes/clubpenguin/__init__.py
+++ app/routes/clubpenguin/__init__.py
@@ -1,7 +1,9 @@
 
 from fastapi import APIRouter
 
+from . import directory
 from . import mobile
 
 router = APIRouter()
+router.include_router(directory.router, prefix="/directory")
 router.include_router(mobile.router, prefix="/mobile")
