27e89e7d55e48768dc1b5c6398ab196e6a61797a
Lekuru <contact@lekuru.xyz>
2025-01-09 17:19:06 +0100

    Add ssl config

--- config.py
+++ config.py
@@ -10,13 +10,17 @@ POSTGRES_DBNAME = os.environ.get('POSTGRES_DBNAME', 'postgres')
 POSTGRES_USER = os.environ.get('POSTGRES_USER', 'postgres')
 POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
 
+WEB_HOST = os.environ.get('WEB_HOST')
+WEB_PORT = int(os.environ.get('WEB_PORT', 80))
+
+SSL_ENABLED = os.environ.get('SSL_ENABLED', 'false').lower() == 'true'
+SSL_KEY_PATH = os.environ.get('SSL_KEY_PATH')
+SSL_CERT_PATH = os.environ.get('SSL_CERT_PATH')
+
 SITE_NAME = os.environ.get('SITE_NAME', 'localhost')
 FROM_EMAIL = os.environ.get('FROM_EMAIL', 'noreply@localhost')
 SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY', '')
 MAX_ACCOUNT_EMAIL = int(os.environ.get('MAX_ACCOUNT_EMAIL', '5'))
-
-WEB_HOST = os.environ.get('WEB_HOST')
-WEB_PORT = int(os.environ.get('WEB_PORT', 80))
 STATIC_KEY = os.environ.get('STATIC_KEY', 'houdini')
 
 USERNAME_FORCE_CASE = os.environ.get('USERNAME_FORCE_CASE', 'false').lower() == 'true'
--- main.py
+++ main.py
@@ -3,7 +3,22 @@
 import uvicorn
 import config
 
-def main():
+def run_ssl():
+    uvicorn.run(
+        "app:api",
+        host=config.WEB_HOST,
+        port=config.WEB_PORT,
+        reload=config.RELOAD,
+        ssl_keyfile=config.SSL_KEY_PATH,
+        ssl_certfile=config.SSL_CERT_PATH,
+        server_header=False,
+        log_config=None,
+    )
+
+def run():
+    if config.SSL_ENABLED:
+        return run_ssl()
+
     # NOTE: Only for debugging; use a wsgi for production
     #       (e.g. uvicorn app:api)
     uvicorn.run(
@@ -16,4 +31,4 @@ def main():
     )
 
 if __name__ == "__main__":
-    main()
+    run()
