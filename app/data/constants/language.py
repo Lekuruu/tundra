4e5c85d6f6705bdcf906c6e3a758d5736bab2ad7
Lekuru <contact@lekuru.xyz>
2025-01-09 21:21:48 +0100

    Add language enum to code conversion

--- app/data/constants/language.py
+++ app/data/constants/language.py
@@ -43,3 +43,51 @@ class SystemLanguage(IntEnum):
     Ukrainian = 38
     Vietnamese = 39
     Unknown = 40
+
+    @property
+    def code(self) -> str:
+        return SystemLanguageCodes.get(self, 'xx')
+
+SystemLanguageCodes = {
+    SystemLanguage.Afrikaans: 'af',
+    SystemLanguage.Arabic: 'ar',
+    SystemLanguage.Basque: 'eu',
+    SystemLanguage.Belarusian: 'be',
+    SystemLanguage.Bulgarian: 'bg',
+    SystemLanguage.Catalan: 'ca',
+    SystemLanguage.Chinese: 'zh',
+    SystemLanguage.Czech: 'cs',
+    SystemLanguage.Danish: 'da',
+    SystemLanguage.Dutch: 'nl',
+    SystemLanguage.English: 'en',
+    SystemLanguage.Estonian: 'et',
+    SystemLanguage.Faroese: 'fo',
+    SystemLanguage.Finnish: 'fi',
+    SystemLanguage.French: 'fr',
+    SystemLanguage.German: 'de',
+    SystemLanguage.Greek: 'el',
+    SystemLanguage.Hebrew: 'he',
+    SystemLanguage.Hugarian: 'hu',
+    SystemLanguage.Icelandic: 'is',
+    SystemLanguage.Indonesian: 'id',
+    SystemLanguage.Italian: 'it',
+    SystemLanguage.Japanese: 'ja',
+    SystemLanguage.Korean: 'ko',
+    SystemLanguage.Latvian: 'lv',
+    SystemLanguage.Lithuanian: 'lt',
+    SystemLanguage.Norwegian: 'no',
+    SystemLanguage.Polish: 'pl',
+    SystemLanguage.Portuguese: 'pt',
+    SystemLanguage.Romanian: 'ro',
+    SystemLanguage.Russian: 'ru',
+    SystemLanguage.SerboCroatian: 'sh',
+    SystemLanguage.Slovak: 'sk',
+    SystemLanguage.Slovenian: 'sl',
+    SystemLanguage.Spanish: 'es',
+    SystemLanguage.Swedish: 'sv',
+    SystemLanguage.Thai: 'th',
+    SystemLanguage.Turkish: 'tr',
+    SystemLanguage.Ukrainian: 'uk',
+    SystemLanguage.Vietnamese: 'vi',
+    SystemLanguage.Unknown: 'xx'
+}
