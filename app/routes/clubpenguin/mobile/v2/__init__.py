c68f22d24c2d096e47fefcda3d3150937cc5a0df
Lekuru <contact@lekuru.xyz>
2025-01-10 01:21:45 +0100

    Add playercard endpoint

--- app/routes/clubpenguin/mobile/v2/__init__.py
+++ app/routes/clubpenguin/mobile/v2/__init__.py
@@ -2,10 +2,12 @@
 from fastapi import APIRouter
 
 from . import authentication
+from . import playercard
 from . import identity
 from . import account
 
 router = APIRouter()
 router.include_router(authentication.router)
+router.include_router(playercard.router)
 router.include_router(identity.router)
 router.include_router(account.router)
---
+++ app/routes/clubpenguin/mobile/v2/playercard.py
@@ -0,0 +1,26 @@
+
+from starlette.authentication import requires
+from fastapi import APIRouter, Request
+from app.models import PlayerCardData
+from app.data import Penguin
+
+router = APIRouter()
+
+@router.get('/card-data', response_model=PlayerCardData)
+@requires('authenticated')
+def player_card(request: Request) -> PlayerCardData:
+    user: Penguin = request.user
+
+    return PlayerCardData(
+        name=user.display_name,
+        coins=user.coins,
+        daysAsMember=30, # TODO
+        badgeLevel=1, # TODO
+        totalLikesReceived=0, # TODO
+        totalLikesGiven=0, # TODO
+        remainingAwardGames=0, # TODO
+        penguinAge=user.penguin_age, # TODO
+        items={}, # TODO
+        outfits={}, # TODO
+        member=True # TODO
+    )
