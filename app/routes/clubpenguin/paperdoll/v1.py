ea19ffaecef66e967b3a66b54fada3f252fd9784
Lekuru <contact@lekuru.xyz>
2025-01-10 23:26:28 +0100

    Cleanup paperdoll endpoint

--- app/routes/clubpenguin/paperdoll/v1.py
+++ app/routes/clubpenguin/paperdoll/v1.py
@@ -1,13 +1,11 @@
 
-from fastapi.responses import RedirectResponse, Response
+from fastapi.responses import RedirectResponse
 from fastapi import APIRouter, Request
-from app.data import penguins
-
 import config
 
 router = APIRouter()
 
-@router.get("/v1/{user_id}/cp")
+@router.get("/v1/{user_id}/cp", response_class=RedirectResponse)
 def avatar_renderer_redirect(
     user_id: int,
     request: Request
