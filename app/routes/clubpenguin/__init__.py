2575176af458928db8182056f3cb9554400490db
Lekuru <contact@lekuru.xyz>
2025-01-10 22:23:47 +0100

    Include paperdoll route

--- app/routes/clubpenguin/__init__.py
+++ app/routes/clubpenguin/__init__.py
@@ -1,9 +1,11 @@
 
 from fastapi import APIRouter
 
+from . import paperdoll
 from . import directory
 from . import mobile
 
 router = APIRouter()
+router.include_router(paperdoll.router, prefix="/paperdoll-renderer")
 router.include_router(directory.router, prefix="/directory")
 router.include_router(mobile.router, prefix="/mobile")
